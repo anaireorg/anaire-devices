9ee6bb5480741fc05dd1c539510827f05921ea10
Antonio López Gracia <alogra@gmail.com>
2020-11-09 13:47:39 +0100

    Add files via upload

---
+++ anaire-device/anaire-device.ino
@@ -0,0 +1,134 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// 20201109 Antonio López Gracia
+// Get CO2, Temperature and humidity measurements and send them to the anaire cloud app
+// MH-Z14A: pin 15 <-> 5Vcc Power In, pin 3 <-> GND, pin 10 <-> UART TXD, pin 18 UART RXD
+// MH-Z14A CO2 sensor connected to Serial1 (pin 18-TX1, pin 19-RX1) in Arduino due
+// Use programming port to get serial monitor working on Arduino IDE (the one close to power input)
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Control Loop timer.
+const int ControlLoopDuration = 5000; // 5 secs
+unsigned long ControlLoopStart = 0;
+
+// Command to get measurements from MH-Z14A CO2 sensor
+byte cmd[9] = {0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79};  
+
+// Command to calibrate MH-Z14A CO2 sensor
+byte cmdCal[9] = {0xFF, 0x01, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0x78};  // calibrate command
+
+// holds the received data from MH-Z14A CO2 sensor
+char response[9]; 
+
+// MH-Z14A CO2 sensor warming time: 3 minutes
+const long CO2WarmingTime = 180000;
+
+// CO2 ppm measured value
+int CO2ppm = 0;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// SETUP
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void setup() {
+
+  // Initialize serial port for serial monitor in Arduino IDE
+  Serial.begin(9600);
+
+  // Initialize serial port to communicate with MH-Z14A CO2 sensor
+  Serial1.begin(9600);
+
+  // Initialize MH-Z14A CO2 sensor: warming up and calibrate
+  Setup_MHZ14A();
+ 
+  // initialize digital pin LED_BUILTIN as an output to blink, showing activity
+  pinMode(LED_BUILTIN, OUTPUT);
+  
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// CONTROL LOOP
+///////////////////////////////////////////////////////////////////////////////////////////////////
+void loop() {
+
+  // gets timestamp for the loop start time
+  ControlLoopStart = millis();
+  
+  // clears out any garbage in the RX buffer
+  while (Serial1.available())  
+  {
+    int garbage = Serial1.read();
+  }
+
+  // sent out read command to the sensor - 9 bytes
+  Serial1.write(cmd, 9);  
+
+  // pauses the sketch and waits for the TX buffer to send all its data to the sensor
+  Serial1.flush();  
+
+  // pauses the sketch and waits for the sensor response
+  while (!Serial1.available())  
+  {
+    delay(0);
+  }
+
+  // once data is avilable, it reads it to a variable 9 bytes
+  Serial1.readBytes(response, 9);  
+  //Serial.println (response);
+
+  // calculates CO2ppm value
+  int responseHigh = (int)response[2];
+  int responseLow = (int)response[3];
+  CO2ppm = (256 * responseHigh) + responseLow;
+
+  //Serial.print (responseHigh);
+  //Serial.print (" <-> ");
+  //Serial.println (responseLow);
+
+  // prints calculated CO2ppm value to serial monitor
+  Serial.print ("CO2ppm = ");
+  Serial.println (CO2ppm);
+
+  //delay(1000);
+
+  // blinks LED to show activity
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(800);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
+
+
+  // Complete time up to ControlLoopTimerDuration
+  while ((millis() - ControlLoopStart) < ControlLoopDuration) 
+  {
+    // waits for 1 sec
+    delay(1000);
+  }
+  
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// FUNCTIONS
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Setup MH-Z14A CO2 sensor
+void Setup_MHZ14A()
+{
+
+  // print info
+  Serial.println ("# Warming up MH-Z14A CO2 sensor...");
+  
+  // Waits for warming time
+  delay(CO2WarmingTime);
+
+  // print info
+  Serial.println ("# Calibrating MH-Z14A CO2 sensor...");
+  
+  // Write calibration command
+  Serial1.write(cmdCal, 9);
+
+  // Waits for 3 seconds
+  delay(3000);
+
+  // print info
+  Serial.println ("# MH-Z14A CO2 sensor setup complete");
+  
+}

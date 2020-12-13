9daa8fea873969af06be026b6ea44518d7f9ec5e
alogracia <alogra@gmail.com>
2020-12-13 19:16:37 +0100

    src code changes

---
+++ src/anaire-device.NodeMCULuaAmicaV2/anaire_config.h
@@ -0,0 +1,12 @@
+// DEVICE
+char anaire_device_id[] = "anaireslim01";      // Unique device id
+int CO2ppm_warning_threshold = 700;                   // Warning threshold
+int CO2ppm_alarm_threshold = 1000;                     // Alarm threshold
+
+// LOCAL CONFIGURATION: WiFi network configuration
+char ssid[] = "anaire";                  // your network SSID (name)
+char password[] = "mel0nc0njam0n";       // your network password (use for WPA, or use as key for WEP)
+
+// CLOUD CONFIGURATION: remote app url
+char cloud_server_address[] = "demo.anaire.org";     // server public IP address
+int cloud_app_port = 30183;                          // cloud application port

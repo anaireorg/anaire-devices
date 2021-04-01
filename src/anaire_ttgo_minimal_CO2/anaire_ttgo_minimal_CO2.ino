57909033d7b8ba404d0a4d3875d475749e716dbf
alogracia <alogra@gmail.com>
2021-04-01 23:08:02 +0200

    Librería Adafruit SCD30 Comportamiento de ambos botones

--- src/anaire_ttgo_minimal_CO2/anaire_ttgo_minimal_CO2.ino
+++ src/anaire_ttgo_minimal_CO2/anaire_ttgo_minimal_CO2.ino
@@ -2,7 +2,10 @@
 // Based on https://developer.sensirion.com/tutorials/create-your-own-co2-monitor/
 // TTGO T-Display board and Sensirion SCD30 CO2, temp and humidity sensor
 // Top button click: toggles buzzer sound
+// Top button double click: performs SCD30 forced calibration
+// Top button triple click: enables self calibration
 // Bottom button click: sleep; click again the button to wake up
+// Bottom button double click: restart device
 
 // TTGO ESP32 board
 #include "esp_timer.h"
@@ -19,8 +22,10 @@
 #define FF99  &SensirionSimple25pt7b
 #define FF90  &ArchivoNarrow_Regular10pt7b
 #define FF95  &ArchivoNarrow_Regular50pt7b
-#include "anaire_ttgo_splash.h" // Anaire splash screen
 TFT_eSPI tft = TFT_eSPI(135, 240); // Invoke library, pins defined in User_Setup.h
+
+// Customized Anaire splash screen
+#include "anaire_ttgo_splash.h" 
 
 // Buttons: Top and bottom considered when USB connector is positioned on the right of the board
 #include "Button2.h"
@@ -30,11 +35,14 @@ Button2 button_top(BUTTON_TOP);
 Button2 button_bottom(BUTTON_BOTTOM);
 
 // Sensirion SCD30 CO2, temperature and humidity sensor
-// Download the SeeedStudio SCD30 Arduino driver here:
-//  => https://github.com/Seeed-Studio/Seeed_SCD30/releases/latest
-#include "SCD30.h"
+// Install Adafruit_SCD30 library from Tools -> Library Manager
+#include <Adafruit_SCD30.h>
+Adafruit_SCD30 scd30;
 #define SCD30_SDA_pin 26  // Define the SDA pin used for the SCD30
 #define SCD30_SCL_pin 27  // Define the SCL pin used for the SCD30
+
+// Bluetooth in TTGO T-Display
+// Install XXX Library
 #include "Sensirion_GadgetBle_Lib.h"  // to connect to Sensirion MyAmbience Android App available on Google Play
 GadgetBle gadgetBle = GadgetBle(GadgetBle::DataType::T_RH_CO2_ALT);
 
@@ -58,31 +66,69 @@ void button_init()
 {
     // Top button click: toggles sound
     button_top.setClickHandler([](Button2 & b) {
-        Serial.println("Top button click: toggle sound");
-        if (sound) {sound = false;}
-        else {sound = true;}
+      Serial.println("Top button click: toggle sound");
+      if (sound) {sound = false;}
+      else {sound = true;}
+    });
+
+    // Top button double click; SCD30 forced recalibration
+    button_top.setDoubleClickHandler([](Button2 & b) {
+      Serial.println("Top button double click: SCD30 calibration");
+      if (!scd30.forceRecalibrationWithReference(400)){
+        Serial.println("Failed to force recalibration at 400ppm");
+      }
+      else {
+        Serial.print("Forced recalibration performed at ");
+        Serial.print(scd30.getForcedCalibrationReference());
+        Serial.println(" ppm");
+      }
+      if (scd30.selfCalibrationEnabled()) {
+        Serial.println("Self calibration enabled");
+      } else {
+        Serial.println("Self calibration disabled");
+      }
+
+    });
+
+    // Top button triple click; SCD30 forced recalibration
+    button_top.setTripleClickHandler([](Button2 & b) {
+      Serial.println("Top button triple click: enable SCD30 self calibration");
+      if (!scd30.selfCalibrationEnabled(true)){
+        Serial.println("Failed to enable self calibration");
+      }
+      if (scd30.selfCalibrationEnabled()) {
+        Serial.println("Self calibration enabled");
+      } else {
+        Serial.println("Self calibration disabled");
+      }
+
     });
     
     // Bottom button click: sleep
     button_bottom.setClickHandler([](Button2 & b) {
-        Serial.println("=> Bottom button click: sleep");
-        int r = digitalRead(TFT_BL);
-        tft.fillScreen(TFT_BLACK);
-        tft.setTextColor(TFT_GREEN, TFT_BLACK);
-        tft.setTextDatum(MC_DATUM);
-        tft.drawString("Press bottom button to wake up",  tft.width() / 2, tft.height() / 2 );
-        espDelay(6000);
-        digitalWrite(TFT_BL, !r);
+      Serial.println("=> Bottom button click: sleep");
+      int r = digitalRead(TFT_BL);
+      tft.fillScreen(TFT_BLACK);
+      tft.setTextColor(TFT_GREEN, TFT_BLACK);
+      tft.setTextDatum(MC_DATUM);
+      tft.drawString("Press bottom button to wake up",  tft.width() / 2, tft.height() / 2 );
+      espDelay(6000);
+      digitalWrite(TFT_BL, !r);
 
-        tft.writecommand(TFT_DISPOFF);
-        tft.writecommand(TFT_SLPIN);
-        //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
-        esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
-        // esp_sleep_enable_ext1_wakeup(GPIO_SEL_0, ESP_EXT1_WAKEUP_ALL_LOW);
-        // set bottom button for wake up
-        esp_sleep_enable_ext0_wakeup(GPIO_NUM_0, 0);
-        delay(200);
-        esp_deep_sleep_start();
+      tft.writecommand(TFT_DISPOFF);
+      tft.writecommand(TFT_SLPIN);
+      //After using light sleep, you need to disable timer wake, because here use external IO port to wake up
+      esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_TIMER);
+      // esp_sleep_enable_ext1_wakeup(GPIO_SEL_0, ESP_EXT1_WAKEUP_ALL_LOW);
+      // set bottom button for wake up
+      esp_sleep_enable_ext0_wakeup(GPIO_NUM_0, 0);
+      delay(200);
+      esp_deep_sleep_start();
+    });
+
+    // Bottom button double click: reset
+    button_bottom.setDoubleClickHandler([](Button2 & b) {
+        ESP.restart();
     });
     
 }
@@ -172,9 +218,31 @@ void setup() {
 
   // Initialize the SCD30 driver
   Wire.begin(SCD30_SDA_pin, SCD30_SCL_pin);
-  scd30.initialize();
-  scd30.setAutoSelfCalibration(true);
-
+  if (!scd30.begin()) {
+    Serial.println("Failed to find SCD30 chip");
+  }
+  else {
+    Serial.println("SCD30 Found!");
+  }
+  
+  /*** Enable or disable automatic self calibration (ASC).
+   * Parameter stored in non-volatile memory of SCD30.
+   * Enabling self calibration will override any previously set
+   * forced calibration value.
+   * ASC needs continuous operation with at least 1 hour
+   * 400ppm CO2 concentration daily.
+   */
+  //if (!scd30.selfCalibrationEnabled(true)){
+  //   Serial.println("Failed to enable or disable self calibration");
+     //while(1) { delay(10); }
+  //}
+  
+  if (scd30.selfCalibrationEnabled()) {
+    Serial.println("Self calibration enabled");
+  } else {
+    Serial.println("Self calibration disabled");
+  }
+  
   // Initialize BUZZER to OFF
   pinMode(BUZZER_GPIO, OUTPUT);
   digitalWrite(BUZZER_GPIO, LOW);
@@ -191,40 +259,42 @@ void setup() {
 
 void loop() {
   
-  float result[3] = {0};
+  //float result[3] = {0};
 
   if (esp_timer_get_time() - lastMmntTime >= startCheckingAfterUs) {
 
-    if (scd30.isAvailable()) {   
-
+    //if (scd30.isAvailable()) {   
+    if (scd30.dataReady()) {
+      
       // Read SCD30
-      scd30.getCarbonDioxideConcentration(result);
-
+      //scd30.getCarbonDioxideConcentration(result);
+      if (!scd30.read()){ 
+        Serial.println("Error reading sensor data"); 
+        return; 
+      }
+      
       // Write measurements to bluetooth
-      gadgetBle.writeCO2(result[0]);
-      gadgetBle.writeTemperature(result[1]);
-      gadgetBle.writeHumidity(result[2]);
+      gadgetBle.writeCO2(scd30.CO2);
+      gadgetBle.writeTemperature(scd30.temperature);
+      gadgetBle.writeHumidity(scd30.relative_humidity);
       gadgetBle.commit();
   
       // Provide the sensor values for Tools -> Serial Monitor or Serial Plotter
       Serial.print("CO2[ppm]:");
-      Serial.print(result[0]);
+      Serial.print(scd30.CO2);
       Serial.print("\t");
-      Serial.print("Temperature[℃]:");
-      Serial.print(result[1]);
+      Serial.print("Temperature[ºC]:");
+      Serial.print(scd30.temperature);
       Serial.print("\t");
       Serial.print("Humidity[%]:");
-      Serial.println(result[2]);
+      Serial.println(scd30.relative_humidity);
   
       // display CO2, temperature and humidity values on the display
-      displayCo2((uint16_t) round(result[0]), result[1], result[2]);
+      displayCo2((uint16_t) round(scd30.CO2), scd30.temperature, scd30.relative_humidity);
       
     }
     
     else {
-      result[0] = 0;
-      result[1] = 0;
-      result[2] = 0;
       Serial.println("Error SCD30");
     }
     

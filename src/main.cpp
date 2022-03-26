0f25a0ed339fc616a6a1a9e8eb9f52e199c2330a
danielbernalb <danielbernalb@gmail.com>
2022-03-26 09:46:43 -0500

    Cambios temporal

--- src/main.cpp
+++ src/main.cpp
@@ -85,6 +85,8 @@ unsigned long errors_loop_start;           // holds a timestamp for each error l
 #include "esp_timer.h"
 #include <Wire.h>
 
+#include <esp_system.h>
+
 // Display and fonts
 #include <TFT_eSPI.h>
 #include <SPI.h>
@@ -208,6 +210,30 @@ void setup()
   }
   Serial.setDebugOutput(true);
 
+  /////////////////////////////////////////////////////////////
+  print_reset_reason(RESET_REASON reason);
+
+  switch ( reason)
+  {
+    case 1 : Serial.println ("POWERON_RESET");break;          /**<1,  Vbat power on reset*/
+    case 3 : Serial.println ("SW_RESET");break;               /**<3,  Software reset digital core*/
+    case 4 : Serial.println ("OWDT_RESET");break;             /**<4,  Legacy watch dog reset digital core*/
+    case 5 : Serial.println ("DEEPSLEEP_RESET");break;        /**<5,  Deep Sleep reset digital core*/
+    case 6 : Serial.println ("SDIO_RESET");break;             /**<6,  Reset by SLC module, reset digital core*/
+    case 7 : Serial.println ("TG0WDT_SYS_RESET");break;       /**<7,  Timer Group0 Watch dog reset digital core*/
+    case 8 : Serial.println ("TG1WDT_SYS_RESET");break;       /**<8,  Timer Group1 Watch dog reset digital core*/
+    case 9 : Serial.println ("RTCWDT_SYS_RESET");break;       /**<9,  RTC Watch dog Reset digital core*/
+    case 10 : Serial.println ("INTRUSION_RESET");break;       /**<10, Instrusion tested to reset CPU*/
+    case 11 : Serial.println ("TGWDT_CPU_RESET");break;       /**<11, Time Group reset CPU*/
+    case 12 : Serial.println ("SW_CPU_RESET");break;          /**<12, Software reset CPU*/
+    case 13 : Serial.println ("RTCWDT_CPU_RESET");break;      /**<13, RTC Watch dog Reset CPU*/
+    case 14 : Serial.println ("EXT_CPU_RESET");break;         /**<14, for APP CPU, reseted by PRO CPU*/
+    case 15 : Serial.println ("RTCWDT_BROWN_OUT_RESET");break;/**<15, Reset when the vdd voltage is not stable*/
+    case 16 : Serial.println ("RTCWDT_RTC_RESET");break;      /**<16, RTC Watch dog reset digital core and rtc module*/
+    default : Serial.println ("NO_MEAN");
+}
+/////////////////////////////////////
+
   // print info
   Serial.println();
   Serial.println("### INIT ANAIRE PiCO2 DEVICE ###########################################");
@@ -245,6 +271,11 @@ void setup()
   // Initialize TTGO board buttons
   Button_Init();
 
+  // Start Captive Portal for 15 seconds
+  PortalFlag = false;
+  Start_Captive_Portal();
+  delay(100);
+
   // Attempt to connect to WiFi network:
   Connect_WiFi();
 
@@ -256,10 +287,6 @@ void setup()
 
   // Initialize and warm up CO2 sensor
   Setup_Sensor();
-
-  // Start Captive Portal for 15 seconds
-  PortalFlag = false;
-  Start_Captive_Portal();
 
   // Init control loops
   measurements_loop_start = millis();

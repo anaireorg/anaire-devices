d55557dfaa08c7a8c09efdd8a30bc2d8e9d41559
danielbernalb <danielbernalb@gmail.com>
2023-10-05 11:28:07 -0500

    Cambios flags

--- src/main.cpp
+++ src/main.cpp
@@ -16,22 +16,22 @@
 
 ////////////////////////////////
 // Modo de comunicaciones del sensor:
-#define Wifi false       // Set to true in case Wifi if desired, Bluetooth off and SDyRTCsave optional
+#define Wifi true        // Set to true in case Wifi if desired, Bluetooth off and SDyRTCsave optional
 #define WPA2 false       // Set to true to WPA2 enterprise networks (IEEE 802.1X)
 #define Rosver false     // Set to true URosario version
 #define Rosver2 false    // Level 2 Urosario version
-#define Bluetooth true   // Set to true in case Bluetooth if desired, Wifi off and SDyRTCsave optional
-#define SDyRTC false      // Set to true in case SD card and RTC (Real Time clock) if desired, Wifi and Bluetooth off
+#define Bluetooth false  // Set to true in case Bluetooth if desired, Wifi off and SDyRTCsave optional
+#define SDyRTC false     // Set to true in case SD card and RTC (Real Time clock) if desired, Wifi and Bluetooth off
 #define SaveSDyRTC false // Set to true in case SD card and RTC (Real Time clock) if desired to save data in Wifi or Bluetooth mode
-#define ESP8285 false    // Set to true for SHT4x sensor
-#define CO2sensor false  // Set to true in case you use a ESP8285 switch
-#define SHT4x true       // Set to true for CO2 sensors: SCD30 and SenseAir S8
+#define ESP8285 false    // Set to true in case you use a ESP8285 switch
+#define CO2sensor false  // Set to true for CO2 sensors: SCD30 and SenseAir S8
+#define SHT4x true       // Set to true for SHT4x sensor
 
 #define SiteAltitude 0   // IMPORTANT for CO2 measurement: Put the site altitude of the measurement, it affects directly the value
 // #define SiteAltitude 2600   // 2600 meters above sea level: Bogota, Colombia
 
 // Escoger modelo de pantalla (pasar de false a true) o si no hay escoger ninguna (todas false):
-#define Tdisplaydisp true
+#define Tdisplaydisp false
 #define OLED66display false
 #define OLED96display false
 
